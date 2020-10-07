188e2386005df86c8c2913c299deadb2d6714f78
Evgeny Kuzmin <odin.dva@bk.ru>
2020-10-08 00:34:58 +0400

    create files

---
+++ less2/HomeWork/hw_02_03.py
@@ -0,0 +1,19 @@
+# 3) Пользователь вводит месяц в виде целого числа от 1 до 12.
+# Сообщить к какому времени года относится месяц (зима, весна, лето, осень).
+# Напишите решения через list и через dict.
+
+
+months = {'Зима': [1, 2, 12], 'Весна': [3, 4, 5], 'Лето': [6, 7, 8], 'Осень': [9, 10, 11]}
+
+while True:
+    try:
+        month = int(input('Введите номер месяца: '))
+        if month < 1 or month > 12:
+            raise ValueError
+        break
+    except ValueError:
+        print('Вы ввели не номер месяца')
+
+for key, val in months.items():
+    if month in val:
+        print(key)
